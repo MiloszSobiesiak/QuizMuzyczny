94d6d98d5b390e1ca658f1e051daad539f9a517e
MiloszSobiesiak <miloszsobiesiak98@gmail.com>
2025-09-14 14:33:43 +0200

    Logout button

--- src/app/modules/home/pages/home-page/home-page.ts
+++ src/app/modules/home/pages/home-page/home-page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SessionsRepository } from '../../../../data-access/sessions/sessions.repository';
 
 @Component({
   selector: 'app-home-page',
@@ -7,8 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './home-page.scss',
 })
 export class HomePage {
+  private readonly sessionsRepository = inject(SessionsRepository);
+
   public logout(): void {
-    localStorage.removeItem('spotify_access_token');
-    sessionStorage.removeItem('spotify_access_token');
+    this.sessionsRepository.logout().subscribe();
   }
 }
