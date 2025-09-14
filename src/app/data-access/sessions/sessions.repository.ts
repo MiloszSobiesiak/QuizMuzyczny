9485d18e365e5294786032bd536fae45437be953
MiloszSobiesiak <miloszsobiesiak98@gmail.com>
2025-09-14 12:57:50 +0200

    Fix url

--- src/app/data-access/sessions/sessions.repository.ts
+++ src/app/data-access/sessions/sessions.repository.ts
@@ -5,9 +5,13 @@ import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class SessionsRepository {
   private readonly http = inject(HttpClient);
-  private readonly apiUrl = 'https://d27946756601.ngrok-free.app/api';
+  private readonly apiUrl = 'https://1b5109c5cc42.ngrok-free.app/api';
 
   public isLoggedIn(): Observable<boolean> {
     return this.http.get<boolean>(`${this.apiUrl}/sessions/is-logged-in`);
+  }
+
+  public logout(): Observable<void> {
+    return this.http.get<void>(`${this.apiUrl}/sessions/logout`);
   }
 }
