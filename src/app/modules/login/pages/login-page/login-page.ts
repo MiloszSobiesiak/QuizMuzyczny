26e71c12f41d331fdc128627d0c315554de90411
MiloszSobiesiak <miloszsobiesiak98@gmail.com>
2025-09-15 21:14:41 +0200

    Add user library read to scope

--- src/app/modules/login/pages/login-page/login-page.ts
+++ src/app/modules/login/pages/login-page/login-page.ts
@@ -11,7 +11,7 @@ export class LoginPage {
     const clientId = '8dc7deb02262449cac03e0ba56ec38aa';
     const redirectUri = 'https://empathetic-youth-production.up.railway.app/api/spotify/callback';
     const scopes =
-      'playlist-read-private playlist-read-collaborative user-read-email user-read-private';
+      'playlist-read-private playlist-read-collaborative user-read-email user-read-private user-library-read';
 
     window.location.href = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${encodeURIComponent(
       scopes
